3af07f6be1b9c6f53434a5684430d44c27318533
uidevperson <32483505+uidevperson@users.noreply.github.com>
2017-11-07 11:58:04 +0000

    es6 modules test

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-alert('main testing');
+import notification from './notification';
+
+console.log(notification.notify, notification.log);
+notification.log('panys');
+notification.notify(' alert testing');
---
+++ src/notification.js
@@ -0,0 +1,13 @@
+function notify(message) {
+    alert(message);
+}
+
+function log(message) {
+    console.log(message);
+}
+
+
+export default {
+    notify: notify,
+    log: log
+};
